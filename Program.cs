d94dfda9b75cd0f671d6d1a4639b093f0a460c09
yagmurozer <yagmur31323@hotmail.com>
2025-03-13 01:17:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// finding is odd or even of a number
+Console.Write("lütfen bir sayı girin:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 % 2 == 1)
+{
+    Console.WriteLine($"{num1} sayısı tektir");
+}
+else
+{
+    Console.WriteLine($"{num1} sayısı çifttir");
+}
+
+// finding is possitive, negative or zero of a number
+Console.Write("lütfen bir sayı girin:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+if (num2 > 0)
+{
+    Console.WriteLine($"{num2} sayısı pozitiftir.");
+}
+else if(num2 < 0)
+{
+    Console.WriteLine($"{num2} sayısı negatiftir.");
+}
+else
+{
+    Console.WriteLine("Sayı sıfıra eşittir");
+}
+
